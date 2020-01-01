f4043be70dab79984db04c77d34f52bec3dbf5ad
Charles Pelton <charliejp0311@gmail.com>
2020-01-01 23:35:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,7 +26,8 @@ def monopoly_with_third_tier
   {
     :railroads => {
       :pieces => 4,
-      :rent_in_dollars => {:owns1 => "",:owns2 => "", :owns3 => "", :owns4 => ""}
+      :rent_in_dollars => {:owns1 => "",:owns2 => "", :owns3 => "", :owns4 => ""},
+      :names => {}
     }
   }
 end
